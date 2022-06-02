de16b5fc11d036f44fa438fd692c332b9a3e5563
jhudsonsg <jhudsonsg@gmail.com>
2022-06-02 20:55:02 -0300

    Configurando leitura de arquivo

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import fs from 'fs';
 import BmlLexer from './src/grammars/BmlLexer.js';
 import BmlParser from './src/grammars/BmlParser.js';
 
-const fileContent = fs.readFileSync('./programa1.bml', 'utf8');
+const [_, __, file] = process.argv;
 
 try {
+    const fileContent = fs.readFileSync(file, 'utf8');
     const chars = new antlr4.InputStream(fileContent);
     const lexer = new BmlLexer(chars);
     const tokens = new antlr4.CommonTokenStream(lexer);
@@ -15,4 +16,6 @@ try {
     parser.programa();
 } catch (error) {
     console.error(error);
+} finally {
+    console.log('Done!');
 }
