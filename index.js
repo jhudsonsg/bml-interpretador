672ba03c0f4646da446915d5ccad82656fe71c8a
jhudsonsg <jhudsonsg@gmail.com>
2022-06-08 21:12:42 -0300

    Criando visitante para vercorrer tokens gerados.

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 import antlr4 from 'antlr4';
 import fs from 'fs';
+import util from 'util';
 
 import BmlLexer from './src/grammars/BmlLexer.js';
 import BmlParser from './src/grammars/BmlParser.js';
 
+import Visitor from './src/grammars/util/Visitor.js';
+
 const [_, __, file] = process.argv;
 
 try {
-    const fileContent = fs.readFileSync(file, 'utf8');
-    const chars = new antlr4.InputStream(fileContent);
-    const lexer = new BmlLexer(chars);
-    const tokens = new antlr4.CommonTokenStream(lexer);
-    const parser = new BmlParser(tokens);
+  const chars = new antlr4.InputStream(fs.readFileSync(file, 'utf8'));
+  const lexer = new BmlLexer(chars);
+  const tokens = new antlr4.CommonTokenStream(lexer);
 
-    parser.programa();
+  const parser = new BmlParser(tokens);
+  parser.buildParseTrees = true;
+  const tree = parser.programa().accept(new Visitor());
+
+  // console.log(util.inspect(tree, { showHidden: false, depth: null, colors: true }));
 } catch (error) {
-    console.error(error);
+  console.error(error);
 } finally {
-    console.log('Done!');
+  console.log('Done!');
 }
---
+++ src/grammars/util/Visitor.js
@@ -0,0 +1,17 @@
+export default class Visitor {
+  visitChildren(ctx) {
+    if (!ctx) {
+      return;
+    }
+
+    if (ctx.children) {
+      return ctx.children.map(child => {
+        if (child.children && child.children.length != 0) {
+          return child.accept(this);
+        } else {
+          return child.getText();
+        }
+      });
+    }
+  }
+}
